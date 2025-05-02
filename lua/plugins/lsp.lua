c08a2a7b566f3881ed363dc5a689508842131c20
Joe Bost <joebost090@gmail.com>
2025-05-01 22:45:28 -0700

    updates

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -32,7 +32,10 @@ return {
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
-      { 'j-hui/fidget.nvim', opts = {} },
+      {
+        'j-hui/fidget.nvim',
+        opts = {},
+      },
 
       -- Allows extra capabilities provided by blink.cmp
       'saghen/blink.cmp',
